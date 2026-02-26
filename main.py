37d24a7a8fcfbf2b6944e627e1e26f8c1a958fb9
julianvlz21 <julianvluna11@gmail.com>
2026-02-26 18:36:42 -0500

    Calculadora de resta (prueba)

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print ("prueba")
+##PRUEBAS PARA GIT
+
+while True:
+    try:
+        print ("\nPrueba \nIngresa dos números para restar")
+        numero_1 = int(input("\nIntroduce el primer número entero: "))
+        numero_2 = int(input("Introduce el segundo número entero: "))
+        resta = numero_1 - numero_2
+        if resta >= 0:
+            print(f"\nLa resta de {numero_1} - {numero_2} es = {resta}")
+            if resta == 13:
+                    print ("¡¡¡¡AYYYYYYYY!!!!")
+
+        elif resta < 0:
+            print(f"\nLa resta de {numero_1} - {numero_2} es = {resta}")
+    except ValueError:
+     print("\nEscribe un número entero CV")
