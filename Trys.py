b33ac1ec67cf98db18006bd62bc8ba5da8631add
julianvlz21 <julianvluna11@gmail.com>
2026-03-26 00:08:19 -0500

    invetario

--- Trys.py
+++ Trys.py
@@ -156,3 +156,110 @@ import random
 # Client management system
 # This module allows registering clients with validation
 
+inventario = []
+contador = 0
+def agregar_productos(contador, nombre, precio, cantidad_):#Se crea un diccioanrio que gruarde los valores para cada producto, para posteriormente agregar el producto a la lista
+    producto = {
+        "id": contador,
+        "nombre": nombre,
+        "precio": precio,
+        "cantidad": cantidad_
+    }
+
+    inventario.append(producto)
+    print("\n\033[1;32m===============¡¡¡Registro exitoso!!!===============\033[0m")
+
+def mostrar_inventario(inventario):
+    if not inventario:
+        print("\n\033[34mInventario vacio\033[0m")
+    else:
+        for producto in inventario:
+            print(f"\033[34m{producto['id']}.\033[0m Producto: {producto['nombre']} | Precio: {producto['precio']} | Cantidad: {producto['cantidad']}")
+
+def calcular_estadisticas(inventario):
+    total_precio = sum(valor['precio']*valor['cantidad']for valor in inventario)
+    print(f"El valor total de los productos: \033[1;32m${total_precio}\033[0m")
+
+    total_inventario = sum(cantidad['cantidad']for cantidad in inventario)
+    print (f"Hay \033[1;32m{total_inventario}\033[0m productos en el inventario")
+
+#Validaciones de errores
+def si_vacio (valor):
+    return valor == ""
+
+def si_0_negativo(valor):
+    return int(valor) <= 0
+
+
+# def validar_datos():
+    # while True:
+        # nombre = input("Nombre: ")
+        # precio = float(input("Precio: "))
+        # cantidad = int(input("Cantidad: "))
+        # try:
+        #     if nombre == "" or precio <= 0 or cantidad <= 0:
+        #         print("Datos erroneos, intente de nuevo")
+        #         continue
+        #     break
+        # except:
+        #     ("\n\033[1,31m¡¡¡Valor invalido, intenta de nuenvo!!!\033[0m")
+        # return nombre, precio, cantidad
+        
+        
+
+
+#menú para usuario
+while True:
+    print("\n\033[1;34m------------------------MENÚ REGISTRO INVENTARIO------------------------\033[0m")
+    print("""
+    \033[1;34m 1.\033[0m Agregar producto
+    \033[1;34m 2.\033[0m Mostrar inventario
+    \033[1;34m 3.\033[0m Calcular estadísticas
+    \033[1;34m 4.\033[0m Salir
+
+""")    
+    opcion = input("Selecciona una de las opciones: ")
+
+    if opcion == "1":
+        print("\033[32m CAPACIDAD PARA 20 PRODUCTOS\033[0m") 
+        cantidad = input("\nCuántos producto va a agregar: ")
+        while not cantidad.isdigit():
+            print("\n\033[1;31m ¡¡¡Valor invalido, intenta de nuevo!!!\033[0m")
+            cantidad = input("\nCuántos producto va a agregar: ")
+            
+        if si_0_negativo (cantidad):
+            print("\n\033[1;31m =====!!!El valor no puede ser 0 o negaitivo¡¡¡=====\033[0m")
+        elif int(cantidad) > 20:
+            print("\033[32m CAPACIDAD PARA 20 PRODUCTOS\033[0m")
+        else:
+            for n in range(int(cantidad)):
+                while True:
+                    nombre = input("\nEscribe el nombre del productos: ")
+                    contador += 1
+                    try:
+                        precio = float(input("Agrega un precio al producto: "))
+                        cantidad_= int(input("Escribe la cantidad de productos |valor entero|: "))
+
+                        if si_vacio(nombre) or si_0_negativo(precio) or si_0_negativo(cantidad_):
+                            print("\n\033[1;31m =====!!!DATO ERRONEO¡¡¡=====\033[0m")
+                            continue
+                        else:
+                            break
+                    except:
+                        print("\n\033[1;31m¡¡¡Valor invalido, intente de nuenvo!!!\033[0m")
+                    
+                agregar_productos(contador, nombre, precio, cantidad_)
+                print(len(inventario))
+    
+    elif opcion == "2":
+        mostrar_inventario(inventario)
+
+    elif opcion == "3":
+        calcular_estadisticas(inventario)
+
+    elif opcion == "4":
+        print ("\033[1;32m================PROGRAMA FINALIZADO================\033[0m")
+        break
+
+    else:
+        print("\n\033[1;31m =====!!!DATO ERRONEO¡¡¡=====\033[0m")
