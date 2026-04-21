527470f87656c39af58aad22dc5a14c581a109a3
julianvlz21 <julianvluna11@gmail.com>
2026-04-21 00:11:01 -0500

    thrid_week_behins

---
+++ html y css/javascript/script.js
@@ -0,0 +1,16 @@
+const imgpets =
+document.getElementById("img-pets");
+
+const sammy =
+document.getElementById("sammy");
+
+
+imgpets.addEventListener("click", () => {
+
+})
+
+
+
+
+
+// https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Selectors/:modal
